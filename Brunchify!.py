91ef5c2e4bcd1f093596cad059ddc03c179c8d39
bcookew <83699317+bcookew@users.noreply.github.com>
2021-11-02 14:58:38 +1300

    Built out menus and input processing

--- Brunchify!.py
+++ Brunchify!.py
@@ -1,26 +1,51 @@
 import shelve
 from time import sleep
+from data_structures import BrunchSpots, SuburbMap
 # User Menus
 def main_menu():
+    header('Main Menu')
     choices = {'about':about, 'search': search_menu, 'quit': end_program}
-    users_choice = user_choice('Please type one of the menu options: ', choices)
+    user_choice('Please type one of the menu options: ', choices)
 
 
 def about():
-    print('''About this app:
-    Brunchify Wellington ''')
+    header('About')
+    print(
+    '''    Brunchify Wellington 
+    ver 1.0
+    Created By: Benjamin C Williams
+
+    Brunchify is a portfolio application for exploring data structures
+    and recall via processing of user input. Information on restaurant
+    opening hours accurate as of 2 Nov 2021''')
+
     main_menu()
 
 
 def search_menu():
-    print('_____Search Menu_____')
+    header('Search Menu')
+    choices = {'search by restaurant name': restaurant_search, 'search by suburb': burb_search}
+    user_choice('Please type one of the menu options: ', choices)
     main_menu()
     
 
 def end_program():
-    print('_____GOODBYE_____')
+    header('GOODBYE')
     sleep(3)
     exit()
+
+def restaurant_search():
+    header('Restaurant Search')
+    
+
+def burb_search():
+    header('Suburb Search')
+    searched_burb = clean_input('Please enter a suburb: ')
+    opts = {'yes':True, 'no':False}
+    adjacent = deduction(opts, clean_input('Would you like to search the adjacent suburbs as well? ', opts))
+    suburb_data.
+    
+    
 
 # Utilities
 def load_data():
@@ -43,27 +68,54 @@ def load_data():
         print('Fatal Error. Data could not be loaded!!!')
         exit()
 
-def clean_input(question):
-    answer = input(question).lower().strip()
+def clean_input(question, options=None):
+    if options == None:
+        answer = input(question).lower().strip()
+    else:
+        answer = input(question + menu_option_formatter(options)).lower().strip()
     if answer =='':
         print("Sorry, we didn't get that.")
         return clean_input(question)
     return answer
 
 def user_choice(question, options):
-    options_list = [option.title() for option in options]
-    options_str = " -- ".join(options_list)
-    answer = input(question + '\n' + options_str + '\n').lower().strip()
+    answer = input(question + menu_option_formatter(options)).lower().strip()
     if answer =='':
         print("Sorry, we didn't get that.")
         return user_choice(question, options)
-    elif answer in options:
-        options[answer]()
     else:
-        print("Sorry, that isn't an option from this menu.")
-        return user_choice(question, options)
+        options[deduction(options, answer)]()
         
-        
+def deduction(options, search_term):
+        potential_matches = []
+        for option in options.keys():
+            for option_index in range(len(option)):
+                match_count = 0
+                for search_index in range(len(search_term)):
+                    if search_index + option_index >= len(option):
+                        continue
+                    elif search_term[search_index] == option[search_index + option_index]:
+                        match_count += 1
+                    else:
+                        break
+                if match_count > 1:
+                    potential_matches.append(option)
+        if len(potential_matches) == 0:
+            new_search_term = clean_input("Sorry, your input didn't match an option from this menu. Please try again:" + menu_option_formatter(options))
+            return deduction(options, new_search_term)
+        elif len(potential_matches) == 1:
+            return potential_matches[0]
+        else:
+            new_search_term = clean_input('Your input matched multiple options please select again.' + menu_option_formatter(potential_matches))
+            return deduction(options, new_search_term)
+    
+def menu_option_formatter(options_list):
+    options_list = [option.title() for option in options_list]
+    option_string = '\n{0}\n'.format(" -- ".join(options_list))
+    return option_string
+
+def header(header):
+    print("\n_____{0}_____\n".format(header))
 
 # Launch Script
 suburb_data, restaurant_data = load_data()
